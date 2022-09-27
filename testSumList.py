883cf80631a7b179a7054ba3e7253d1505402ae3
partyotter12345 <amaanrazi20@gmail.com>
2022-09-26 22:10:48 -0500

    Update testSumList.py

--- testSumList.py
+++ testSumList.py
@@ -10,6 +10,10 @@ def sumList(nums):
 def main():
     nums = [5,2,4,7]
     print(sumList(nums))
+    nums = [4,524,42,562]
+    print(sumList(nums))
+    nums = [3,5,63,733]
+    print(sumList(nums))
     nums = []
     print(sumList(nums))
 
