d2051a6cab966f2e6d5074453c05cdbc0f4a04d0
partyotter12345 <amaanrazi20@gmail.com>
2022-09-26 21:10:46 -0500

    Create quotient.py

---
+++ quotient.py
@@ -0,0 +1,9 @@
+#Amaan Razi HMWK #1
+#quotient.py
+
+x=int(input("Enter the first integer"))
+y=int(input("Enter the second integer"))
+v = x//y
+z = x%y
+
+print('The quotient of ' , x , ' and ' , y , ' is ' , v , ' with a remainder of ' , z , ' . ' , sep= '')
