6507f881a7d6d26aa40e37fcf3f5eae448338990
partyotter12345 <amaanrazi20@gmail.com>
2022-09-26 22:41:48 -0500

    Create numProduct.py

---
+++ numProduct.py
@@ -0,0 +1,13 @@
+#Amaan Razi HMWK #1
+#numProduct.py
+
+
+def product(nums):
+    prod = 1
+    for n in nums:
+        
+        prod = prod*n
+        
+    return prod
+
+print(product([5, 4, 6]))
