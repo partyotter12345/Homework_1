5ecef3b9b1b43a13a0e0cced270cca2540cfcbab
partyotter12345 <amaanrazi20@gmail.com>
2022-09-26 21:04:56 -0500

    Create add3.py

---
+++ add3.py
@@ -0,0 +1,10 @@
+#Amaan Razi, HMWK #1
+#add3.py
+
+x = int(input("Enter an integer : "))
+y = int(input("Enter another integer : "))
+z = int(input("Enter another integer : "))
+
+sum = x+y+z
+
+print('The sum of ', x, ' , ' ,y, ' and ' ,z, ' is ' ,sum ,'.', sep = '')
